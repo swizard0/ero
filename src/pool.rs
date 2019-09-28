f037b1bd66b13b6cd6e0fb156fa3b063fb08bdcc
Alexey Voznyuk <me@swizard.info>
2019-09-28 23:12:50 +0300

    `bootstrap` and `state` in master process support

--- src/pool.rs
+++ src/pool.rs
@@ -38,31 +38,42 @@ use super::{
     ErrorSeverity,
 };
 
-pub fn spawn_pool<MN, SN, B, FB, EB, BS, S, H, FH, EH, M, MT, ST, I>(
+pub fn spawn_pool<MN, SN, MB, FMB, EMB, BMS, MS, C, FC, EC, SB, FSB, ESB, BSS, SS, H, FH, EH, MT, ST, I>(
     supervisor: &supervisor::Supervisor,
     master_params: Params<MN>,
-    master_converter: M,
-    slaves_bootstrap: B,
+    master_init_state: BMS,
+    master_bootstrap: MB,
+    master_converter: C,
+    slaves_bootstrap: SB,
     slaves_handler: H,
     slaves_iter: I,
 )
     -> mpsc::Sender<MT>
 where MN: AsRef<str> + Send + 'static,
       SN: AsRef<str> + Send + 'static,
-      M: Fn(MT) -> ST + Send + 'static,
+      MB: Fn(BMS) -> FMB + Send + 'static,
+      FMB: IntoFuture<Item = MS, Error = ErrorSeverity<BMS, EMB>> + 'static,
+      FMB::Future: Send,
+      EMB: Debug + Send + 'static,
+      BMS: Send + 'static,
+      MS: Send + 'static,
+      C: Fn(MT, MS) -> FC + Send + 'static,
+      FC: IntoFuture<Item = (ST, MS), Error = ErrorSeverity<BMS, EC>> + 'static,
+      FC::Future: Send,
+      EC: Debug + Send + 'static,
       MT: Send + 'static,
       ST: Send + 'static,
-      B: Fn(BS) -> FB + Sync + Send + 'static,
-      FB: IntoFuture<Item = S, Error = ErrorSeverity<BS, EB>> + 'static,
-      FB::Future: Send,
-      EB: Debug + Send + 'static,
-      S: Send + 'static,
-      BS: Send + 'static,
-      H: Fn(ST, S) -> FH + Sync + Send + 'static,
-      FH: IntoFuture<Item = S, Error = ErrorSeverity<BS, EH>> + 'static,
+      SB: Fn(BSS) -> FSB + Sync + Send + 'static,
+      FSB: IntoFuture<Item = SS, Error = ErrorSeverity<BSS, ESB>> + 'static,
+      FSB::Future: Send,
+      ESB: Debug + Send + 'static,
+      SS: Send + 'static,
+      BSS: Send + 'static,
+      H: Fn(ST, SS) -> FH + Sync + Send + 'static,
+      FH: IntoFuture<Item = SS, Error = ErrorSeverity<BSS, EH>> + 'static,
       FH::Future: Send,
       EH: Debug + Send + 'static,
-      I: IntoIterator<Item = (Params<SN>, BS)>,
+      I: IntoIterator<Item = (Params<SN>, BSS)>,
 {
     let (tasks_tx, tasks_rx) = mpsc::channel(1);
     let (slaves_tx, slaves_rx) = mpsc::channel(1);
@@ -70,10 +81,16 @@ where MN: AsRef<str> + Send + 'static,
     supervisor.spawn_link(
         run_master(
             master_params,
+            master_init_state,
+            master_bootstrap,
             tasks_rx,
             slaves_rx,
             master_converter,
         ).map_err(|error| match error {
+            MasterError::Bootstrap(error) =>
+                error!("state bootstrap error in master: {:?}", error),
+            MasterError::Converter(error) =>
+                error!("tasks converter error in master: {:?}", error),
             MasterError::TasksChannelDropped =>
                 error!("tasks channel dropped in master"),
             MasterError::TasksChannelDepleted =>
@@ -82,6 +99,8 @@ where MN: AsRef<str> + Send + 'static,
                 error!("slaves channel dropped in master"),
             MasterError::SlavesChannelDepleted =>
                 info!("slaves channel depleted in master"),
+            MasterError::SlaveTaskChannelDropped =>
+                error!("slave task channel dropped in master"),
             MasterError::CrashForced =>
                 info!("crash forced after error in master"),
         })
@@ -119,133 +138,165 @@ where MN: AsRef<str> + Send + 'static,
 }
 
 #[derive(Debug)]
-pub enum MasterError {
+pub enum MasterError<EB, EC> {
+    Bootstrap(EB),
+    Converter(EC),
     TasksChannelDropped,
     TasksChannelDepleted,
     SlavesChannelDropped,
     SlavesChannelDepleted,
+    SlaveTaskChannelDropped,
     CrashForced,
 }
 
-pub fn run_master<N, MT, ST, M>(
+pub fn run_master<N, B, FB, EB, BS, C, FC, EC, S, MT, ST>(
     params: Params<N>,
+    init_state: BS,
+    bootstrap: B,
     tasks_rx: mpsc::Receiver<MT>,
     slaves_rx: mpsc::Receiver<oneshot::Sender<ST>>,
-    converter: M,
+    converter: C,
 )
-    -> impl Future<Item = (), Error = MasterError>
+    -> impl Future<Item = (), Error = MasterError<EB, EC>>
 where N: AsRef<str>,
-      M: Fn(MT) -> ST,
+      B: Fn(BS) -> FB,
+      FB: IntoFuture<Item = S, Error = ErrorSeverity<BS, EB>>,
+      C: Fn(MT, S) -> FC,
+      FC: IntoFuture<Item = (ST, S), Error = ErrorSeverity<BS, EC>>,
 {
-    struct RestartableState<MT, ST, M> {
+    struct RestartableState<BS, B, MT, ST, C> {
+        state: BS,
+        bootstrap: B,
         tasks_rx: mpsc::Receiver<MT>,
         slaves_rx: mpsc::Receiver<oneshot::Sender<ST>>,
-        converter: M,
+        converter: C,
     }
 
     restart::restartable(
         params,
         RestartableState {
+            state: init_state,
+            bootstrap,
             tasks_rx,
             slaves_rx,
             converter,
         },
-        |RestartableState { tasks_rx, slaves_rx, converter, }| {
-            enum Source<A, B> {
-                Task(A),
-                Slave(B),
-            }
+        |RestartableState { state: init_state, bootstrap, tasks_rx, slaves_rx, converter, }| {
+            bootstrap(init_state)
+                .into_future()
+                .then(move |bootstrap_result| match bootstrap_result {
+                    Ok(state) => {
+                        enum Source<A, B> {
+                            Task(A),
+                            Slave(B),
+                        }
 
-            struct State<ST, M> {
-                pending: Vec<ST>,
-                slaves: Vec<oneshot::Sender<ST>>,
-                converter: M,
-            }
+                        struct State<S, B, ST, C> {
+                            state: S,
+                            bootstrap: B,
+                            pending: Vec<ST>,
+                            slaves: Vec<oneshot::Sender<ST>>,
+                            converter: C,
+                        }
 
-            let blender = Blender::new()
-                .add(tasks_rx)
-                .add(slaves_rx)
-                .finish_sources()
-                .fold(Source::Slave, Source::Slave)
-                .fold(Source::Task, Source::Task)
-                .finish();
+                        let blender = Blender::new()
+                            .add(tasks_rx)
+                            .add(slaves_rx)
+                            .finish_sources()
+                            .fold(Source::Slave, Source::Slave)
+                            .fold(Source::Task, Source::Task)
+                            .finish();
 
-            let state = State {
-                pending: Vec::new(),
-                slaves: Vec::new(),
-                converter,
-            };
+                        let state = State {
+                            state,
+                            bootstrap,
+                            pending: Vec::new(),
+                            slaves: Vec::new(),
+                            converter,
+                        };
 
-            loop_fn((blender, state), |(blender, mut state)| {
-                blender
-                    .then(move |blend_result| match blend_result {
-                        Ok((Source::Task(master_task), blender)) => {
-                            let task = (state.converter)(master_task);
-                            match state.slaves.pop() {
-                                Some(slave_tx) =>
-                                    match slave_tx.send(task) {
-                                        Ok(()) =>
-                                            Ok(Loop::Continue((blender, state))),
-                                        Err(_task) => {
-                                            let (tasks_rx, (slaves_rx, ())) = blender.decompose();
-                                            Err(ErrorSeverity::Recoverable {
-                                                state: RestartableState {
-                                                    tasks_rx,
-                                                    slaves_rx,
-                                                    converter: state.converter,
+                        let future = loop_fn((blender, state), |(blender, mut state)| {
+                            blender
+                                .then(move |blend_result| match blend_result {
+                                    Ok((Source::Task(master_task), blender)) => {
+                                        let State { state, bootstrap, mut pending, mut slaves, converter, } = state;
+                                        let future = converter(master_task, state)
+                                            .into_future()
+                                            .then(move |converter_result| match converter_result {
+                                                Ok((task, state)) => match slaves.pop() {
+                                                    Some(slave_tx) =>
+                                                        match slave_tx.send(task) {
+                                                            Ok(()) => {
+                                                                let state = State { state, bootstrap, pending, slaves, converter, };
+                                                                Ok(Loop::Continue((blender, state)))
+                                                            },
+                                                            Err(_task) =>
+                                                                Err(ErrorSeverity::Fatal(MasterError::SlaveTaskChannelDropped)),
+                                                        },
+                                                    None => {
+                                                        pending.push(task);
+                                                        let state = State { state, bootstrap, pending, slaves, converter, };
+                                                        Ok(Loop::Continue((blender, state)))
+                                                    },
                                                 },
-                                            })
-                                        }
-                                    },
-                                None => {
-                                    state.pending.push(task);
-                                    Ok(Loop::Continue((blender, state)))
-                                },
-                            }
-                        },
-                        Ok((Source::Slave(slave_online_tx), blender)) => {
-                            match state.pending.pop() {
-                                Some(task) =>
-                                    match slave_online_tx.send(task) {
-                                        Ok(()) =>
-                                            Ok(Loop::Continue((blender, state))),
-                                        Err(_task) => {
-                                            let (tasks_rx, (slaves_rx, ())) = blender.decompose();
-                                            Err(ErrorSeverity::Recoverable {
-                                                state: RestartableState {
-                                                    tasks_rx,
-                                                    slaves_rx,
-                                                    converter: state.converter,
+                                                Err(ErrorSeverity::Fatal(error)) =>
+                                                    Err(ErrorSeverity::Fatal(MasterError::Converter(error))),
+                                                Err(ErrorSeverity::Recoverable { state, }) => {
+                                                    let (tasks_rx, (slaves_rx, ())) = blender.decompose();
+                                                    Err(ErrorSeverity::Recoverable {
+                                                        state: RestartableState {
+                                                            state, bootstrap, tasks_rx, slaves_rx, converter,
+                                                        },
+                                                    })
                                                 },
-                                            })
-                                        }
+                                            });
+                                        Either::A(future)
                                     },
-                                None => {
-                                    state.slaves.push(slave_online_tx);
-                                    Ok(Loop::Continue((blender, state)))
-                                },
-                            }
-                        },
-                        Err(Source::Task(ErrorEvent::Depleted {
-                            decomposed: DecomposeZip { left_dir: (_slaves_rx, ()), myself: Gone, right_rev: (), },
-                        })) =>
-                            Err(ErrorSeverity::Fatal(MasterError::TasksChannelDepleted)),
-                        Err(Source::Slave(ErrorEvent::Depleted {
-                            decomposed: DecomposeZip { left_dir: (), myself: Gone, right_rev: (_tasks_rx, ()), },
-                        })) =>
-                            Err(ErrorSeverity::Fatal(MasterError::SlavesChannelDepleted)),
-                        Err(Source::Task(ErrorEvent::Error {
-                            error: (),
-                            decomposed: DecomposeZip { left_dir: (_slaves_rx, ()), myself: Gone, right_rev: (), },
-                        })) =>
-                            Err(ErrorSeverity::Fatal(MasterError::TasksChannelDropped)),
-                        Err(Source::Slave(ErrorEvent::Error {
-                            error: (),
-                            decomposed: DecomposeZip { left_dir: (), myself: Gone, right_rev: (_tasks_rx, ()), },
-                        })) =>
-                            Err(ErrorSeverity::Fatal(MasterError::SlavesChannelDropped)),
-                    })
-            })
+                                    Ok((Source::Slave(slave_online_tx), blender)) => {
+                                        let future_result = match state.pending.pop() {
+                                            Some(task) =>
+                                                match slave_online_tx.send(task) {
+                                                    Ok(()) =>
+                                                        Ok(Loop::Continue((blender, state))),
+                                                    Err(_task) =>
+                                                        Err(ErrorSeverity::Fatal(MasterError::SlaveTaskChannelDropped)),
+                                                },
+                                            None => {
+                                                state.slaves.push(slave_online_tx);
+                                                Ok(Loop::Continue((blender, state)))
+                                            },
+                                        };
+                                        Either::B(result(future_result))
+                                    },
+                                    Err(Source::Task(ErrorEvent::Depleted {
+                                        decomposed: DecomposeZip { left_dir: (_slaves_rx, ()), myself: Gone, right_rev: (), },
+                                    })) =>
+                                        Either::B(result(Err(ErrorSeverity::Fatal(MasterError::TasksChannelDepleted)))),
+                                    Err(Source::Slave(ErrorEvent::Depleted {
+                                        decomposed: DecomposeZip { left_dir: (), myself: Gone, right_rev: (_tasks_rx, ()), },
+                                    })) =>
+                                        Either::B(result(Err(ErrorSeverity::Fatal(MasterError::SlavesChannelDepleted)))),
+                                    Err(Source::Task(ErrorEvent::Error {
+                                        error: (),
+                                        decomposed: DecomposeZip { left_dir: (_slaves_rx, ()), myself: Gone, right_rev: (), },
+                                    })) =>
+                                        Either::B(result(Err(ErrorSeverity::Fatal(MasterError::TasksChannelDropped)))),
+                                    Err(Source::Slave(ErrorEvent::Error {
+                                        error: (),
+                                        decomposed: DecomposeZip { left_dir: (), myself: Gone, right_rev: (_tasks_rx, ()), },
+                                    })) =>
+                                        Either::B(result(Err(ErrorSeverity::Fatal(MasterError::SlavesChannelDropped)))),
+                                })
+                        });
+                        Either::A(future)
+                    },
+                    Err(ErrorSeverity::Fatal(error)) =>
+                        Either::B(result(Err(ErrorSeverity::Fatal(MasterError::Bootstrap(error))))),
+                    Err(ErrorSeverity::Recoverable { state, }) =>
+                        Either::B(result(Err(ErrorSeverity::Recoverable {
+                            state: RestartableState { state, bootstrap, tasks_rx, slaves_rx, converter, },
+                        }))),
+                })
         })
         .map_err(|restartable_error| match restartable_error {
             restart::RestartableError::Fatal(error) =>
