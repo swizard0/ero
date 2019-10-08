b26373075230c5ce53abb2b908f0aa88b53f7c39
Alexey Voznyuk <me@swizard.info>
2019-10-08 15:52:47 +0300

    write FSM by hand instead of combinators

--- src/restart.rs
+++ src/restart.rs
@@ -1,11 +1,11 @@
-use std::time::Instant;
+use std::{
+    mem,
+    time::Instant,
+};
 
 use futures::{
-    future::{
-        self,
-        loop_fn,
-        Either,
-    },
+    Poll,
+    Async,
     Future,
     IntoFuture,
 };
@@ -19,7 +19,6 @@ use log::{
 use tokio::timer::Delay;
 
 use super::{
-    Loop,
     Params,
     ErrorSeverity,
     RestartStrategy,
@@ -31,56 +30,105 @@ pub enum RestartableError<E> {
     RestartCrashForced,
 }
 
-pub fn restartable<N, S, F, I, T, E>(
+pub struct RestartableFuture<N, F, FI, S> {
     params: Params<N>,
-    state: S,
-    mut restartable_fn: F,
-)
-    -> impl Future<Item = T, Error = RestartableError<E>>
+    restartable_fn: F,
+    mode: RestartableMode<S, FI>,
+}
+
+enum RestartableMode<S, FI> {
+    Invalid,
+    Invoke { state: S, },
+    Poll { future: FI, },
+    Delay { future: Delay, state: S, },
+}
+
+impl<N, S, F, I, T, E> Future for RestartableFuture<N, F, I::Future, S>
 where F: FnMut(S) -> I,
       I: IntoFuture<Item = T, Error = ErrorSeverity<S, E>>,
       N: AsRef<str>,
 {
-    loop_fn((params, state), move |(params, state)| {
-        restartable_fn(state)
-            .into_future()
-            .then(move |result| {
-                match result {
-                    Ok(value) => {
-                        debug!("{} normal termination", params.name.as_ref());
-                        let future = future::result(Ok(Loop::Break(value)));
-                        Either::A(future)
-                    },
-                    Err(ErrorSeverity::Recoverable { state, }) =>
-                        match params.restart_strategy {
-                            RestartStrategy::InstantCrash => {
-                                info!("recoverable error in {} but current strategy is to crash", params.name.as_ref());
-                                let future = future::result(Err(RestartableError::RestartCrashForced));
-                                Either::A(future)
-                            },
-                            RestartStrategy::RestartImmediately => {
-                                info!("recoverable error in {}, restarting immediately", params.name.as_ref());
-                                let future = future::result(Ok(Loop::Continue((params, state))));
-                                Either::A(future)
-                            },
-                            RestartStrategy::Delay { restart_after, } => {
-                                info!("recoverable error in {}, restarting in {:?}", params.name.as_ref(), restart_after);
-                                let future = Delay::new(Instant::now() + restart_after)
-                                    .then(|delay_result| {
-                                        if let Err(error) = delay_result {
-                                            error!("restart delay timer error: {:?}, restarting anyway", error);
-                                        }
-                                        Ok(Loop::Continue((params, state)))
-                                    });
-                                Either::B(future)
-                            },
+    type Item = T;
+    type Error = RestartableError<E>;
+
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        loop {
+            match mem::replace(&mut self.mode, RestartableMode::Invalid) {
+                RestartableMode::Invalid =>
+                    panic!("cannot poll RestartableFuture twice"),
+
+                RestartableMode::Invoke { state, } => {
+                    debug!("RestartableMode::Invoke");
+                    let future = (self.restartable_fn)(state)
+                        .into_future();
+                    self.mode = RestartableMode::Poll { future, };
+                },
+
+                RestartableMode::Poll { mut future, } => {
+                    debug!("RestartableMode::Poll");
+                    match future.poll() {
+                        Ok(Async::NotReady) => {
+                            self.mode = RestartableMode::Poll { future, };
+                            return Ok(Async::NotReady);
                         },
-                    Err(ErrorSeverity::Fatal(error)) => {
-                        debug!("{} fatal error", params.name.as_ref());
-                        let future = future::result(Err(RestartableError::Fatal(error)));
-                        Either::A(future)
-                    },
-                }
-            })
-    })
+                        Ok(Async::Ready(item)) =>
+                            return Ok(Async::Ready(item)),
+                        Err(ErrorSeverity::Recoverable { state, }) =>
+                            match self.params.restart_strategy {
+                                RestartStrategy::InstantCrash => {
+                                    info!("recoverable error in {} but current strategy is to crash", self.params.name.as_ref());
+                                    return Err(RestartableError::RestartCrashForced);
+                                },
+                                RestartStrategy::RestartImmediately => {
+                                    info!("recoverable error in {}, restarting immediately", self.params.name.as_ref());
+                                    self.mode = RestartableMode::Invoke { state, };
+                                },
+                                RestartStrategy::Delay { restart_after, } => {
+                                    info!("recoverable error in {}, restarting in {:?}", self.params.name.as_ref(), restart_after);
+                                    let future = Delay::new(Instant::now() + restart_after);
+                                    self.mode = RestartableMode::Delay { future, state, };
+                                },
+                            },
+                        Err(ErrorSeverity::Fatal(error)) => {
+                            debug!("{} fatal error", self.params.name.as_ref());
+                            return Err(RestartableError::Fatal(error));
+                        },
+                    }
+                },
+
+                RestartableMode::Delay { mut future, state, } => {
+                    debug!("RestartableMode::Delay");
+                    match future.poll() {
+                        Ok(Async::NotReady) => {
+                            self.mode = RestartableMode::Delay { future, state, };
+                            return Ok(Async::NotReady);
+                        },
+                        Ok(Async::Ready(())) =>
+                            self.mode = RestartableMode::Invoke { state, },
+                        Err(error) => {
+                            error!("restart delay timer error: {:?}, restarting anyway", error);
+                            self.mode = RestartableMode::Invoke { state, };
+                        },
+                    }
+                },
+            }
+        }
+    }
+}
+
+pub fn restartable<N, S, F, I, T, E>(
+    params: Params<N>,
+    state: S,
+    restartable_fn: F,
+)
+    -> RestartableFuture<N, F, I::Future, S>
+where F: FnMut(S) -> I,
+      I: IntoFuture<Item = T, Error = ErrorSeverity<S, E>>,
+      N: AsRef<str>,
+{
+    RestartableFuture {
+        params,
+        restartable_fn,
+        mode: RestartableMode::Invoke { state, },
+    }
 }
